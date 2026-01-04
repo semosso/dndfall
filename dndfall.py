37b9581ce5570e8c79d60fc43b828733ed265b08
Vinicius Azambuja <vnazambuja@gmail.com>
2026-01-04 20:46:46 -0300

    finished extracting and tagging funcitons

--- dndfall.py
+++ dndfall.py
@@ -1,51 +1,57 @@
+# todo: only working on spells for now
+from curses import raw
+
 import json  # for data intake from SRD
 import re  # for info extraction
 
-# get JSON data, currently a selected number of spells from a local json
+# PART 1, getting JSON data (selected number of spells from local json)
 # todo: API calls to 5e SRD database, see https://5e-bits.github.io/docs/
-with open(file="raw_spells.json", mode="r", encoding="utf-8") as raw_json:
-    RAW_SPELLS: list[dict] = json.load(raw_json)
+with open(
+    file="/home/vinicius/projects/dndfall/raw_spells.json", mode="r", encoding="utf-8"
+) as raw_json:
+    raw_spells: list[dict] = json.load(raw_json)
 
-
-# normalize JSON intake into a list of dicts, with my schema
-# todo: 1, implement schemas for monsters etc.
-# 2, typeddict in the future, to typehint each field
-# 3, lambdas not only but also to avoid aliasing raw <> normalized data
+# PART 2, templating how data should look: raw (JSON above), normalized and finally curated
+# normalized vs. curated: curated is barebones + tags extracted from normalized
+# field information (e.g., school, duration), stays in normalized and should be queriable. TBD if this makes sense.
+# P.s., multiple lambdas not only, but also to avoid aliasing between dicts
 # (cont) I kept them in non mutables for consistency, but TBD
-def normalize_json(db: list) -> list:
-    FIELDS_TO_KEEP: dict = {
-        "index": lambda x: x,  #
-        "name": lambda x: x,
-        "level": lambda x: x,
-        "concentration": lambda x: x,
-        "ritual": lambda x: x,
-        "school": lambda x: x["name"],
-        "range": lambda x: x,
-        "components": lambda x: list(x),  # do I need this to avoid aliasing?
-        "material": lambda x: x if "GP" in x else None,
-        "duration": lambda x: x,
-        "casting_time": lambda x: x,
-        "classes": lambda x: [c["name"] for c in x],
-        "higher_level": lambda x: (True, x) if x else False,
-        # how could I change this to "description" and still get the data?
-        # printed as \n sometimes, why?
-        "desc": lambda x: "\n".join([s.strip() for s in x]),
-    }
+NORMALIZED_SCHEME: dict = {
+    "index": lambda x: x,  # do I want this?
+    "name": lambda x: x,
+    "level": lambda x: x,
+    "concentration": lambda x: x,
+    "ritual": lambda x: x,
+    "school": lambda x: x["name"],
+    "range": lambda x: x,
+    "components": lambda x: list(x),  # do I need this?
+    "material": lambda x: x if "GP" in x else None,
+    "duration": lambda x: x,
+    "casting_time": lambda x: x,
+    "classes": lambda x: [c["name"] for c in x],
+    "higher_level": lambda x: (True, x) if x else False,
+    # how could I change this to "description" and still get the data ("desc" in JSON)?
+    "desc": lambda x: " ".join([s.strip() for s in x]),
+}
 
-    normalized_db: list[dict] = []
-    for spell in RAW_SPELLS:
-        normalized_spell: dict = {}
-        for key, fn in FIELDS_TO_KEEP.items():
+
+# works on any structure to move it forward; raw to normalized, normalized to curated
+def normalizing_JSON(
+    raw_json: list = raw_spells, norm_format: dict = NORMALIZED_SCHEME
+) -> list:
+    normalized_spells: list[dict] = []
+    for spell in raw_json:
+        n_spell: dict = {}
+        for key, fn in norm_format.items():
             if key in spell:
-                normalized_spell[key] = fn(spell[key])
-        normalized_db.append(normalized_spell)
+                n_spell[key] = fn(spell[key])
+        normalized_spells.append(n_spell)
+    return normalized_spells
 
-    return normalized_db
 
-
-norm_spells: list[dict] = normalize_json(RAW_SPELLS)
-
-# extracting relevant information (i.e., not in fields)
+# PART 3A, deciding which information should be extracted from description
+# Fields vs. extration: level or school are fields; conditions, damage type etc. have to be extracted
+# Anything extracted should be added as internal tags, queriable with specific syntax
 CONDITIONS: set[str] = {
     "blinded",
     "charmed",
@@ -60,7 +66,8 @@ CONDITIONS: set[str] = {
 }
 
 DAMAGE_TYPE: set[str] = {
-    ## TBD if I'll have to add non-magical (bludgeoning, slashing, piercing)
+    # TBD, will I have to add non-magical (bludgeoning, slashing, piercing)?
+    # add "no_damage" as a tag, or do it via search syntax (e.g., != damage)
     "acid",
     "cold",
     "fire",
@@ -80,54 +87,74 @@ TARGET: set[str] = {
     "any_number_of_creatures",
 }
 
-SAVING_THROW: set[str] = {"str", "dex", "con", "wis", "int", "cha"}
-
-REGEX_PATTERNS: dict = {
-    "conditions": [(r"\b{cond}").format(cond=cond) for cond in CONDITIONS],
-    "damage_type": [
-        (r"\b[0-9]+d[0-9]+(\+[0-9]+)? {dmg} damage").format(dmg=dmg)  # melhor \d+ ?
-        for dmg in DAMAGE_TYPE
-    ],
-    "target": [],
-    "saving_throw": [],
-}
-
-
-# loops through spell description, finds info, and creates dict of tags
-def extract_info(spells: list, regex_pattern: dict):
-    matches: dict = {}
-    for spell in spells:
-        spell_keywords: list = []
-        for cond in REGEX_PATTERNS["conditions"]:  # next, expand for all patterns
-            match = re.search(cond, spell["desc"])
-            if match:
-                spell_keywords.append(match.group())
-                matches[spell["name"]] = spell_keywords
-    return matches
-
-
-print(extract_info(norm_spells, REGEX_PATTERNS))
-# print(extract_info(norm_spells, CONDITIONS))
-
-# # def tagging(spells: list, trackables: set):
-# #     for spell in spells:
-
-
-# # for con in CONDITIONS:
-# #     print(extract_info(norm_spells, con))
+SAVING_THROW: set[str] = {
+    "strength",
+    "dexterity",
+    "constitution",
+    "wisdom",
+    "intelligence",
+    "charisma",
+}  # on extract, better to look for "has to make a * saving throw",
+# and then divide? or better to pull from here?
 
 TAG_RULES: dict = {
-    "conditions": (),
-    "damage_type": (),
-    "average_dmg": (),
-    "aoe": (),
-    "saving_throw": (),
+    "conditions": [(r"\b{cond}").format(cond=cond) for cond in CONDITIONS],
+    "damage_type": [
+        (r"\b[0-9]+d[0-9]+(\+[0-9]+)? {dmg} damage").format(dmg=dmg)
+        # melhor \d+?
+        # just {dmg} damage should do it
+        for dmg in DAMAGE_TYPE
+    ],
+    # "target": [],
+    # "saving_throw": [],
+    # "teleport": [],
+    # "healing": [], "heal XX points", "regain" in cure
+    # "buff": [], # bless-like, "adds the number"; receives immunity, resistance etc.
+    # "debuff": [], # bane-like, "subtracts the number"
+    # "restoration": [] # ends disease, condition etc.
+    # "adv/disav": [] # gives one or the other
+    # "average_damage": hardcode average
+    #
 }
 
-# print(TAG_RULES)
 
-# # # for spell in spells:
-# # #     tags:dict = {}
-# # #     for tag, rule in TAG_RULES.items():
-# # #         if rule in spell["desc"]:
-# # #             spell[tag] =
+# PART 3B: actual extraction mechanism: loops through NORMALIZED spell description, returns tags
+# extract() works on normalized dict, tag() initializes and works on curated one -- seems OK
+# I actually don't care about the return other than for the tag function. maybe nest it there?
+def extract_tags(spell: dict, search_pattern: dict):
+    tags: set = set()
+    description: str = spell["desc"].lower()
+    for key, rule in TAG_RULES.items():
+        for pattern in rule:
+            if match := re.search(pattern=pattern, string=description):
+                tags.add((key, match.group()))  # TBD if I want the key
+    return tags
+
+
+# PART 3C: puts together the curated dict; what should it look like?
+# TBH I don't care. NORMALIZED is the source of truth (actually not, but close)
+# CURATED is opinion based; what matters are my opinions, the tags I think are relevant to index for query
+# all the FACTUAL stuff is still accessible through the normalized (e.g., defined fields)
+# this can "easily" be used with other categories, monsters etc. (create a pattern for each)
+def curate_spells(normalized_spells: list[dict], patterns: dict):
+    curated_spells: list[dict] = []
+    for spell in normalized_spells:
+        c_spell: dict = {
+            "name": spell["name"],
+            "level": spell["level"],
+            "school": spell["school"],
+            "tags": extract_tags(spell, patterns),
+        }
+        curated_spells.append(c_spell)
+    return curated_spells
+
+
+norm: list[dict] = normalizing_JSON()
+print(curate_spells(normalized_spells=norm, patterns=TAG_RULES))
+
+# PART 4, search engine, replicating scryfall's formal syntax idea
+# e.g., l:/level: (<, <=, \==, >=, >, !=), c:/condition: (T/F, \==, fuzzy later, !=), etc.
+
+
+# PART 5, user interface, still a long way away from having to think about this
+# TBD what information will be shown; e.g., list of spells that meet the criteria? links to SRD?
