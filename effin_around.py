7a173e57f900951c941f163660a7fc7932be2cd7
Vinicius Azambuja <vnazambuja@gmail.com>
2026-01-16 18:35:29 -0300

    more notes on searching MVPs

--- effin_around.py
+++ effin_around.py
@@ -2,7 +2,7 @@
 import re
 import rich
 import json
-from enum import StrEnum # sugestão Takai
+from enum import StrEnum  # sugestão Takai
 
 from normalization import normalizing_spells
 from indexing import create_indices
@@ -16,22 +16,24 @@ spells: dict[str, dict] = normalizing_spells(raw_spells)
 indices: dict = create_indices(spells)
 
 
-## MVP of searching routines; "level" only at first 
+## MVP of searching routines; "level" only at first
+
 
 # categorizing the operators
 class NumericOperators(StrEnum):
-    EQ = "==" # = would be more natural for users
+    EQ = "=="  # = would be more natural for users
     N_EQ = "!="
     HT_o_E = ">="
     HT = ">"
     LT_o_E = "<="
     LT = "<"
 
-    # SearchCommand gets the string operator from QueryParser
-    # SearchCommand calls get_operator with the string operator, gets the applicable operation
-    # maybe SearchCommand also has to pass the first and second param
-    def get_operator(self, str_operator):
+    # SearchCommand gets the string operator from QueryParser, and the type (numeric, text etc.)
+    # SearchCommand calls get_operator, gets the applicable operation
+    # SearchCommand has to pass the str operator, and maybe the arguments as well?
+    def get_operator(self, str_op):
         pass
+
 
 class TextOperators(StrEnum):
     IS = "=="
@@ -42,10 +44,8 @@ class BooleanOperators(StrEnum):
     IS = "=="
     I_N = "!="
 
-# categorizing the operations from the operators
 
-# 
-
+# where should this live? inside each Operator class? Operator class, with specific subclasses?
 def compare(x, y):
     return x >= y  # IF THIS CHANGES
 
@@ -54,15 +54,20 @@ def compare(x, y):
 user_input = "level:>=8"  # IF THIS CHANGES
 
 # MVP of parser: takes user input, breaks it into category, operator, query
-parsing_collon: list = re.split(pattern=r"(:)", string=user_input)
-rich.print("first break is: ", parsing_collon)  # 0 before, 1 :, 2 command and query
-parsing_operator: list = [
-    s for s in re.split(pattern=r"(>=)", string=parsing_collon[2]) if s
+gets_command: list = re.split(pattern=r"(:)", string=user_input)
+rich.print("first break is: ", gets_command)  # 0 before, 1 :, 2 command and query
+command = gets_command[0]
+
+gets_operator: list = [
+    s for s in re.split(pattern=r"(>=)", string=gets_command[2]) if s
 ]  # IF THIS CHANGES
-rich.print("second break is: ", parsing_operator)
+rich.print("second break is: ", gets_operator)
+operator = gets_operator[0]
+content = gets_operator[1]
+
 
 # MVP of SearchCommand: takes pieces from parser, composes command
-full_command: list = [parsing_collon[0], parsing_operator[0], parsing_operator[1]]
+full_command: list = [command, operator, content]
 
 # MVP of SearchEngine: takes command, applies to indices
 for k, v in indices[full_command[0]].items():  # k=3, v=spells
